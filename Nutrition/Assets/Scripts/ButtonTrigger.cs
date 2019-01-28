220733a3277fc7554faf7690a91baeb8be3bad01
Anthony Ostia <rehddog@gmail.com>
2019-01-28 14:26:29 -0800

    Added button

---
+++ Nutrition/Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour {
+
+    public int count;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Button")
+        {
+            Debug.Log(count);
+        }
+    }
+}
