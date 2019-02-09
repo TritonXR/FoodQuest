c2bc3ff5c4909cdb339f3e83e93562325aa4ab88
Jeremy K Luong <jkluong@UCSD.EDU>
2019-02-08 20:57:23 -0800

    files

---
+++ Nutrition/Assets/Scripts/grab.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grab : MonoBehaviour {
+    
+    bool isGrabbed = false;
+    GameObject controllerScript;
+  //  SteamVR_TrackedController controller = controllerScript.GetComponent<SteamVR_TrackedController>();
+
+    // Use this for initialization
+    void Start () {
+        //SteamVR_TrackedController controller = controllerScript.GetComponent<SteamVR_TrackedController>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	//	if(controller.gripped)
+    //    {
+
+    //    }
+	}
+
+    public void test()
+    {
+      //  if (SteamVR_TrackedCamera.triggerPressed) ;
+    }
+    public void attach(GameObject obj)
+    {
+        if (isGrabbed)
+        {
+          //  obj.transform.SetParent(controller);
+
+        }
+        else
+        {
+            obj.transform.SetParent(null);
+        }
+    }
+}
