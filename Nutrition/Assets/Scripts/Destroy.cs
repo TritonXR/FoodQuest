7773f1c367dde8448e1b1947fc8004331bd72bac
Anthony Ostia <rehddog@gmail.com>
2019-02-13 18:31:04 -0800

    Added attack

---
+++ Nutrition/Assets/Scripts/Destroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    public int Health;
+
+
+    void Start()
+    {
+        Health = 100; 
+    }
+    void OnCollisionEnter(Collision otherObj)
+    {
+        if (otherObj.gameObject.tag == "Weapon")
+        {
+            Health = Health - 50;
+            Debug.Log("I have been damaged");
+        }
+    }
+
+    void Update()
+    {
+       if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
