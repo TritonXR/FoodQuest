be562bb47db3a93c32981abec267c2efb68676f8
Anthony A Ostia <aostia@UCSD.EDU>
2019-02-15 14:01:42 -0800

    Added shield

--- Nutrition/Assets/Scripts/Destroy.cs
+++ Nutrition/Assets/Scripts/Destroy.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour {
 
-    public int Health;
+    public int Enemy_Health;
 
 
     void Start()
     {
-        Health = 100; 
+        Enemy_Health = 100; 
     }
     void OnCollisionEnter(Collision otherObj)
     {
         if (otherObj.gameObject.tag == "Weapon")
         {
-            Health = Health - 50;
-            Debug.Log("I have been damaged");
+            Enemy_Health = Enemy_Health - 50;
+            Debug.Log("Enemy has been damaged");
         }
     }
 
     void Update()
     {
-       if (Health <= 0)
+       if (Enemy_Health <= 0)
         {
             Destroy(gameObject);
+            Debug.Log("Enemy has been slayed");
         }
     }
 }
