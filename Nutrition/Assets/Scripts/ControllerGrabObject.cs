a1f98ab22fdf01ff84f22037e2cdcb4f1207a3cf
Jeanne P Vu <jpv002@UCSD.EDU>
2019-02-22 18:28:25 -0800

    Added some stuff

---
+++ Nutrition/Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+
+    // 1
+    private GameObject collidingObject;
+    // 2
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+        // 1
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        // 2
+        collidingObject = col.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
