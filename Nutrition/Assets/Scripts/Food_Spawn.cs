3af8b2855b5fc8c4dbb262c5ca3dacd86b0cb35b
Anthony Ostia <rehddog@gmail.com>
2019-03-07 17:56:29 -0800

    added food respawn

--- Nutrition/Assets/Scripts/Food_Spawn.cs
+++ Nutrition/Assets/Scripts/Food_Spawn.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Food_Spawn : MonoBehaviour {
 
-    public GameObject Food;
+    public GameObject food;
 
     void OnTriggerExit(Collider other)
     {
+        string name = other.gameObject.name;
+
         if (other.gameObject.tag == "Bread")
-            Instantiate(Food);
+        {
+            Instantiate(food, transform.position, transform.rotation);
+        }
+            
     }
 }
