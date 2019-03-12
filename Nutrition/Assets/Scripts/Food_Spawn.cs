2c5906d97f02f9a879604e23e2b1720ff7b7d064
Anthony Ostia <rehddog@gmail.com>
2019-03-12 10:57:24 -0700

    Fixed rotation

--- Nutrition/Assets/Scripts/Food_Spawn.cs
+++ Nutrition/Assets/Scripts/Food_Spawn.cs
@@ -13,8 +13,22 @@ public class Food_Spawn : MonoBehaviour {
 
         if (other.gameObject.tag == "Big Bread")
         {
-            Instantiate(food, transform.position, transform.rotation);
+            Instantiate(food, transform.position, Quaternion.Euler(-90, 0, 0));
         }
-            
+
+        if (other.gameObject.tag == "Seed Roll")
+        {
+            Instantiate(food, transform.position, Quaternion.Euler(0, 0, 65));
+        }
+
+        if (other.gameObject.tag == "Croissant")
+        {
+            Instantiate(food, transform.position, Quaternion.Euler(0, 0, -90));
+        }
+
+        if (other.gameObject.tag == "Danish")
+        {
+            Instantiate(food, transform.position, Quaternion.Euler(0, 0, 90));
+        }
     }
 }
