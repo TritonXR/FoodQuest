0bd2ca66dae03cef484af73f6709352f87963043
Anthony Ostia <rehddog@gmail.com>
2019-04-19 13:13:04 -0700

    Working on behavior

---
+++ Nutrition/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Transform player;
+    NavMeshAgent nav;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+
+}
+
