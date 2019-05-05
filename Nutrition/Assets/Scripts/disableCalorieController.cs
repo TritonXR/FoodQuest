d940ac5384a4d69d53ff8bd43ddd7618f2101e3a
Brian <b2choy@ucsd.edu>
2019-05-05 00:42:35 -0700

    Added numpad enable/disable for calorie game

---
+++ Nutrition/Assets/Scripts/disableCalorieController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableCalorieController : MonoBehaviour {
+
+    private disableCalorie[] calObjects;
+
+    private void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start () {
+        calObjects = Object.FindObjectsOfType<disableCalorie>();
+    }
+	
+	// Update is called once per frame
+    // TODO: Separate game object active methods for when new games come
+	void Update () {
+		if(Input.GetKeyDown("[1]"))
+        {
+            Debug.Log("Remove calorie game");
+            foreach (disableCalorie test in calObjects)
+            {
+                test.gameObject.SetActive(false);
+            }
+        }
+        if (Input.GetKeyDown("[2]"))
+        {
+            Debug.Log("Set Calorie game");
+            foreach (disableCalorie test in calObjects)
+            {
+                test.gameObject.SetActive(true);
+            }
+        }
+    }
+}
