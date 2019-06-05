0e205b186e9ea8c84bb86c875752ab0be84c00ec
Anthony <aostia@ucsd.edu>
2019-06-05 16:06:06 -0700

    Fixed script

--- Nutrition/Assets/Scripts/Inventory.cs
+++ Nutrition/Assets/Scripts/Inventory.cs
@@ -7,61 +7,106 @@ public class Inventory : MonoBehaviour {
 
     public GameObject food;
     //the actual image sprites to be displayed
-    public static GameObject apple;
-    public static GameObject fish;
-    public static GameObject carrot;
-    public static GameObject broc;
-    public static GameObject grape;
-
+    public GameObject apple;
+    public  GameObject fish;
+    public  GameObject carrot;
+    public  GameObject broc;
+    public  GameObject grape;
+    private GameObject randomFood;
     Dictionary<GameObject, int> items = new Dictionary<GameObject, int>();
     //public static Inventory instance;
 
     void Start () {
         //not sure about this
         //apple = GameObject.F
-        //how to grab the foodz indGameObjectWithTag("food");
-        
-	}
+        //how to grab the foodz
+        // findGameObjectWithTag("food");
+        randomFood = null;
+
+    }
 
     void OnCollisionEnter(Collision otherObj)
     {
 
-        if (otherObj.gameObject.tag == "Weapon")
+        if (otherObj.gameObject.tag == "Weapon" || otherObj.gameObject.tag == "Rage Sword")
         {
-            Debug.Log("food is destroyed!!!!!!!!!!!!!!!!!!!");
-            food.SetActive(false);
-            Destroy(food);
-            Debug.Log("food is destroyed " + food.activeSelf);
-            addItem();
+
+            food.SetActive(true);
+            Debug.Log("food is " + food.activeSelf);
+            if (food != null)
+            {
+                Debug.Log("food is destroyed!!!!!!!!!!!!!!!!!!!");
+                food.SetActive(false);
+                //Destroy(food);
+                //Debug.Log("food is destroyed " + food.activeSelf);
+                addItem();
+            }
         }
     }
 
     public GameObject pickRandomItem()
     {
         //generate random number and choose an item to add 
-        var randomInt = Random.Range(1, 6);
-        var randomFood = foodsList[randomInt];
-        return randomFood;
+        var randomInt = Random.Range(1, 5);
+
+        switch (randomInt)
+        {
+            case 1:
+                randomFood = apple;
+                break;
+            case 2:
+                randomFood = fish;
+                break;
+            case 3:
+                randomFood = carrot;
+                break;
+            case 4:
+                randomFood = broc;
+                break;
+            case 5:
+                randomFood = grape;
+                break;
+        }
+        return fish;
+
+
     }
 
-    
+    public bool CompareObjs(GameObject compareToEach)
+    {
+
+        foreach (var item in items.Keys)
+        {
+            if (compareToEach.GetInstanceID() == item.GetInstanceID())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+
     public void addItem()
     {
-        //if the item has never been added
+        //if the item has  been added
         GameObject foodName = pickRandomItem();
-        if ((items.ContainsKey(foodName)))
+        Debug.Log("item chosen: " + foodName.name);
+        if (CompareObjs(foodName) == true)
         {
             Debug.Log("incrementing item");
             items[foodName]++;
         }
-        //if the item is already in it
-        else if ((!items.ContainsKey(foodName))) {
+        //if the item is not already in it
+        if (!items.ContainsKey(foodName)) {
             //add the sprite
             Debug.Log("adding item");
-            //activate panel for first time
-            foodName = GameObject.Find(foodName.name);
             foodName.SetActive(true);
             items.Add(foodName, 1);
+            Debug.Log("now has " + items[(foodName)] + " things called " + foodName.name );
         }
    
 
@@ -83,18 +128,18 @@ public class Inventory : MonoBehaviour {
 
 
     // our "data base :P" of food, their sprites and stats
-    Dictionary<int, GameObject> foodsList = new Dictionary<int, GameObject>()
-    {
-        {1, apple},
-        {2, fish},
-        {3, carrot},
-        {4, broc},
-        {5, grape},
-        //{"Bun", 20 },
-        //{"Tomato", 50 },
-        //{"Lettuce", 15 },
-        //{"Meat", 300 },
-        //{"Cheese", 150 },
-        //{"Apple", 75 }
-    };
+    //Dictionary<int, GameObject> foodsList = new Dictionary<int, GameObject>()
+    //{
+    //    {1, apple},
+    //    {2, fish},
+    //    {3, carrot},
+    //    {4, broc},
+    //    {5, grape},
+    //    {"Bun", 20 },
+    //    {"Tomato", 50 },
+    //    {"Lettuce", 15 },
+    //    {"Meat", 300 },
+    //    {"Cheese", 150 },
+    //    {"Apple", 75 }
+    //};
 }
