31f0708661dbfe702704dbae77b952b4b5d865fa
Brian <b2choy@ucsd.edu>
2019-06-08 02:43:54 -0700

    added text to macro molecule game

---
+++ Nutrition/Assets/Scripts/MacroText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MacroText : MonoBehaviour {
+
+    private string origText;
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        origText = "\n3 Oranges = 1 Cup of Juice";
+        text = GetComponent<Text>();
+
+        //Reset text on switching/starting macro game
+        if(origText != text.text)
+        {
+            text.text = origText;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void updateCupText(int fullCup, int partFilledCup)
+    {
+        text.text = "\nYou have made " + fullCup + " " + partFilledCup + "/3 " + "cups of orange juice.";
+    }
+}
