a279e88263465108db6dbbe19996905af284991b
Anthony Ostia <rehddog@gmail.com>
2020-01-20 23:12:59 -0800

    Working on inventory

--- Nutrition/Assets/Scripts/Item.cs
+++ Nutrition/Assets/Scripts/Item.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour {
 
     public static int tomato = 0;
+    public GameObject tomatoPic;
+
+
+    void Start()
+    {
+        tomatoPic.SetActive(false);
+        Dictionary<int, string> food = new Dictionary<int, string>();
+        food.Add(1, "Tomato");
+    }
 
 
     void OnCollisionEnter(Collision otherObj)
@@ -14,6 +24,7 @@ public class Item : MonoBehaviour {
             tomato += 1;
             Destroy(otherObj.gameObject);
             Debug.Log(tomato);
+            tomatoPic.SetActive(true);
         }
     }
 }
