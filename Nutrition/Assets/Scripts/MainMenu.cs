d07808457239c969c6ac500df06c5bca53578126
Anthony Ostia <rehddog@gmail.com>
2020-02-04 22:12:42 -0800

    Working on main menu

---
+++ Nutrition/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject panel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Button pressed");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(2);
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        panel.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        panel.SetActive(false);
+    }
+}
