2d06206e93276f7707064b4680b622b6fdba91b3
Anthony Ostia <rehddog@gmail.com>
2020-02-09 22:12:27 -0800

    working in tutorial

---
+++ Nutrition/Assets/classDecision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classDecision : MonoBehaviour {
+
+    public GameObject warriorPortal;
+    public GameObject magePortal;
+
+    void Start()
+    {
+        warriorPortal.SetActive(false);
+        magePortal.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.tag == "Player")
+        {
+            warriorPortal.SetActive(true);
+            magePortal.SetActive(true);
+        }
+    }
+}
