5a0c89de15427d285827786fde2b42d4ecbc5d54
e1ha <59583520+e1ha@users.noreply.github.com>
2020-02-12 18:55:04 -0800

    Added the CreateObjects Script

---
+++ Nutrition/Assets/Scripts/CreateObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour {
+
+	public Transform Spawnpoint; 
+	public Rigidbody Prefab;
+	public float delay = 0.75f; 
+	
+	public void repeat() {
+		InvokeRepeating("faucetOn", 0, delay); 
+	}
+	
+	// when faucet on button is pressed
+	public void faucetOn () {
+		Rigidbody RigidPrefab; 
+		//create water droplets
+		RigidPrefab = Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation) as Rigidbody; 
+	
+		
+	}
+	
+	public void faucetOff () {
+			CancelInvoke(); 
+
+	}
+	
+	
+} 
