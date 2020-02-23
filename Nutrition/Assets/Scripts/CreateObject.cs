761f82a8e606c7ae69c17887456ec17c98419be4
e1ha <59583520+e1ha@users.noreply.github.com>
2020-02-22 23:32:55 -0800

    added steam in pot

--- Nutrition/Assets/Scripts/CreateObject.cs
+++ Nutrition/Assets/Scripts/CreateObject.cs
@@ -7,9 +7,13 @@ public class CreateObject : MonoBehaviour {
 	public Transform Spawnpoint; 
 	public GameObject Prefab;
 	public float delay = 0.75f; 
-	
+	private float waterStartTime; 
+	private float waterEndTime;
+	public GameObject water; 
+	public GameObject pot; 
 	public void Repeat()
-    {
+    {	
+		waterStartTime = Time.time; 
 		InvokeRepeating("FaucetOn", 0, delay); 
 	}
 	
@@ -19,6 +23,16 @@ public class CreateObject : MonoBehaviour {
         //create water droplets
 		GameObject RigidPrefab = Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation) as GameObject;
         Destroy(RigidPrefab, 1);
+		waterEndTime = Time.time; 
+
+		/* if (waterEndTime - waterStartTime > 5f) {
+			//pot = GameObject.Find("Pot"); 
+			water = GameObject.FindObjectsOfTypeAll(WaterInPot);
+			water.SetActive(true); 
+		}*/
+
+
+		
 	}
 	
 	public void FaucetOff ()
