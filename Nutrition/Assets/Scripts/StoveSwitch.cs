0bc686fb3ae77e9e499d221cde76c69cdd90f870
Anthony Ostia <rehddog@gmail.com>
2020-02-22 18:40:40 -0800

    stove stuff

--- Nutrition/Assets/Scripts/StoveSwitch.cs
+++ Nutrition/Assets/Scripts/StoveSwitch.cs
@@ -6,11 +6,19 @@ public class StoveSwitch : MonoBehaviour {
 
 	public GameObject lightt;
 	public GameObject lSwitch;
-
+    public GameObject fire;
 	public GameObject _outPosition;
 	public GameObject _inPosition;
 
-	void Awake()
+    private bool switchOn = true;
+
+    void Start()
+    {
+        lightt.SetActive(false);
+        fire.SetActive(false);
+    }
+
+    void Awake()
 	{
 		_inPosition = GameObject.FindWithTag ("InPosition");
 		_outPosition = GameObject.FindWithTag ("OutPosition");
@@ -21,13 +29,24 @@ public class StoveSwitch : MonoBehaviour {
 	{
 		if (other.gameObject.tag == "leftHand" || other.gameObject.tag == "rightHand") 
 		{
-			lightt.SetActive (true);
-			lSwitch.transform.position = new Vector3 (_inPosition.transform.position.x,
-				_inPosition.transform.position.y,
-				_inPosition.transform.position.z);
+            if (switchOn)
+            {
+                lightt.SetActive(true);
+                fire.SetActive(true);
+                lSwitch.transform.position = new Vector3(_inPosition.transform.position.x, _inPosition.transform.position.y,
+                    _inPosition.transform.position.z);
+                switchOn = false;
+            }
 
+
+            else
+            {
+                lightt.SetActive(false);
+                fire.SetActive(false);
+                lSwitch.transform.position = new Vector3(_outPosition.transform.position.x, _outPosition.transform.position.y,
+                    _outPosition.transform.position.z);
+                switchOn = true;
+            }
 		}
-
 	}
-
 }
