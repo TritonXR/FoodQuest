5fc28732d9e84b8cf2915d7707073c1bf97e3539
Anthony Ostia <rehddog@gmail.com>
2020-02-25 07:12:14 -0800

    mino

--- Nutrition/Assets/MinotaurBehavior.cs
+++ Nutrition/Assets/MinotaurBehavior.cs
@@ -8,28 +8,69 @@ public class MinotaurBehavior : MonoBehaviour {
     public Transform player;
     public float speed;
     public float angularSpeed;
-    public float singleStep;
-    private float chargeStamp;
-    public float chargeCooldown = 5.0f;
+    private float singleStep;
+    private float locationStamp = 0.0f;
+    private float chargeStamp = 0.0f;
+    public float chargeCooldown = 3.0f;
+    public float locationCooldown = 3.0f;
     private Vector3 targetDirection;
 
-    public Animator anime;
+    private bool locationBoolean;
+    private bool chargeBoolean;
 
     // Use this for initialization
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         enemy = GetComponent<Rigidbody>();
+        locationBoolean = true;
+        chargeBoolean = false;
     }
 
     // Update is called once per frame
     void Update ()
     {
-        targetDirection = player.position - transform.position;
-        singleStep = angularSpeed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, targetDirection, speed);
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-        transform.rotation = Quaternion.LookRotation(newDirection);
-        anime.SetFloat("Speed", singleStep);
+        if (locationBoolean)
+        {
+            chargeStamp = 0;
+
+           while(locationStamp <= locationCooldown)
+           {
+                targetDirection = player.position - transform.position;
+                singleStep = angularSpeed * Time.deltaTime;
+                Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+                transform.rotation = Quaternion.LookRotation(newDirection);
+           }
+
+            if(locationStamp <= 3)
+            {
+                locationBoolean = false;
+                chargeBoolean = true;
+            }
+
+            locationStamp += Time.deltaTime;
+        }
+
+
+        else if (chargeBoolean)
+        {
+            locationStamp = 0;
+
+            while (chargeStamp <= chargeCooldown)
+            {
+                enemy.AddRelativeForce(Vector3.forward * speed);
+            }
+
+
+            if(chargeStamp <= 3)
+            {
+                locationBoolean = true;
+                chargeBoolean = false;
+            }
+
+            chargeStamp += Time.deltaTime;
+        }
+        Debug.Log(chargeStamp);
+        chargeStamp += Time.deltaTime;
     }
 }
