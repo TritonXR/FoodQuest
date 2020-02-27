ed9cd181164c9d5ed367deb2f7aee097142adbc6
Anthony Ostia <rehddog@gmail.com>
2020-02-27 11:25:33 -0800

    kitchen

---
+++ Nutrition/Assets/Scripts/WaterFilling.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFilling : MonoBehaviour {
+
+    private int count;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        count = 0;
+        transform.localScale = new Vector3(2, 0.1f, 2);
+        transform.localPosition = new Vector3(0, 0.1f, 0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Water")
+        {
+            if(count < 3)
+            {
+                transform.localScale = new Vector3(2, transform.localScale.y + 0.1f, 2);
+                transform.localPosition = new Vector3(0, transform.localScale.y + 0.1f, 0);
+                count += 1;
+            }
+
+        }
+    }
+}
