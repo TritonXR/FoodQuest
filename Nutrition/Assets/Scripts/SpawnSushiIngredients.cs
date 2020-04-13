222fa0f76637dbe8b21b0578c3058a5910f2043b
e1ha <59583520+e1ha@users.noreply.github.com>
2020-04-13 00:30:41 -0700

    Added Sushi Mat

---
+++ Nutrition/Assets/Scripts/SpawnSushiIngredients.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSushiIngredients : MonoBehaviour {
+
+	public GameObject salmon;
+	public GameObject avocado; 
+	public GameObject cucumber; 
+
+	//other is the object colliding into the collider
+	void OnTriggerEnter(Collider other) {
+		salmon.SetActive(true); 
+		avocado.SetActive(true); 
+		cucumber.SetActive(true); 
+	}
+}
